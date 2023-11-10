795a3689bdc1c36c5b7e077de56508af7c16cb62
Manju <manjubhargavi342@gmail.com>
2023-11-10 15:11:26 +0530

    final

--- src/Product.js
+++ src/Product.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Product = ({ name, price, addToCart,image }) => {
   return (
     <div className="product">
-      <img src={`../images/${image}`} alt={name} />
+      <img src={`images/${image}`} alt={name} />
       <h3>{name}</h3>
       <p>${price}</p>
       <button onClick={() => addToCart({ name, price })}>Add to Cart</button>
